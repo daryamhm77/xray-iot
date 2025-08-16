ac454efd33ffcc9b825157bcf4a0409e8411880f
daryamhm77 <daryamhm77@gmail.com>
2025-08-16 19:12:29 +0330

    feat(database): add database module and abstract schema
    - Add DatabaseModule with MongoDB connection
    - Add AbstractDocument base schema class
    - Configure async database connection

---
+++ libs/common/src/database/abstract.schema.ts
@@ -0,0 +1,8 @@
+import { Prop, Schema } from '@nestjs/mongoose';
+import { Types } from 'mongoose';
+
+@Schema()
+export class AbstractDocument {
+  @Prop({ type: Types.ObjectId, default: () => new Types.ObjectId() })
+  _id: Types.ObjectId | string;
+}
